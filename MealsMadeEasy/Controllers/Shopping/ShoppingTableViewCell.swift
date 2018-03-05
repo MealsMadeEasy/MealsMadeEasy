4338dd5c54a86ba88132698b04678cdf9b284bf1
ttran94 <taintran94@gmail.com>
2018-03-04 20:54:27 -0500

    Added Shopping View Controller and ShoppingTableCell Controller

---
+++ MealsMadeEasy/Controllers/Shopping/ShoppingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingTableViewCell.swift
+//  MealsMadeEasy
+//
+//  Created by Tai Tran on 3/4/18.
+//  Copyright © 2018 Tai Tran. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
